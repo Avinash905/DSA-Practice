75d4d6303d7ebd037ab891f0618b278759567cc6
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-09-11 04:16:24 +0530

    Added solution - LeetHub

---
+++ Elements in the Range - GFG/elements-in-the-range.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+		// Your code goes here
+		// TC - O(n)
+        // SC - O(1)
+        if(n<B-A+1) return false;
+        
+        for(int i=0;i<n;i++){
+            if(abs(arr[i])>=A && abs(arr[i])<=B){
+                int z=abs(arr[i])-A;
+                if(arr[z]>0)arr[z]*=-1;
+            }
+        }
+        int count=0;
+        for(int i=0;i<=B-A && i<n;i++){
+            if(arr[i]<=0)count++;
+            else false;
+        }
+        return count==(B-A+1);
+	}
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends

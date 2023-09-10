6fef32a452e5d3d404b4c68c5727d2fc1e500a95
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-09-11 04:17:13 +0530

    Added solution - LeetHub

--- Elements in the Range - GFG/elements-in-the-range.cpp
+++ Elements in the Range - GFG/elements-in-the-range.cpp
@@ -15,14 +15,11 @@ class Solution{
         if(n<B-A+1) return false;
         
         for(int i=0;i<n;i++){
-            if(abs(arr[i])>=A && abs(arr[i])<=B){
-                int z=abs(arr[i])-A;
-                if(arr[z]>0)arr[z]*=-1;
-            }
+            if(abs(arr[i])>=A && abs(arr[i])<=B && arr[abs(arr[i])-A]>0)arr[abs(arr[i])-A]*=-1;
         }
         int count=0;
         for(int i=0;i<=B-A && i<n;i++){
-            if(arr[i]<=0)count++;
+            if(arr[i]<0)count++;
             else false;
         }
         return count==(B-A+1);

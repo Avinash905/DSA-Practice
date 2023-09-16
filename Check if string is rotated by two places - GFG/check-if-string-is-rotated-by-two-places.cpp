8923f891bdfcef37e9c1623514df8e496915aadf
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-09-16 17:31:45 +0530

    Added solution - LeetHub

--- Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.cpp
+++ Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.cpp
@@ -11,33 +11,36 @@ class Solution
     //another string by exactly 2 places.
     bool antiClk(string str1, string str2)
     {
-        // Your code here
-        string s=str1;
         int n=str1.size();
         char first=str1[0],second=str1[1];
+        
+        if(str2[n-2]!=first && str2[n-1]!=second)return false;
+        
         for(int i=0;i<n-2;i++){
-            s[i]=str1[i+2];
+            if(str2[i]!=str1[i+2])return false;
         }
-        s[n-2]=first;
-        s[n-1]=second;
-        return s==str2;
+
+        return true;
     }
     bool clk(string str1, string str2)
     {
-        // Your code here
-        string s=str1;
         int n=str1.size();
         char first=str1[n-1],second=str1[n-2];
-        for(int i=n-1;i>=0;i--){
-            s[i]=str1[i-2];
+        
+        if(str2[1]!=first && str2[0]!=second)return false;
+        
+        for(int i=2;i<n;i++){
+            if(str2[i]!=str1[i-2])return false;
         }
-        s[0]=second;
-        s[1]=first;
-        return s==str2;
+
+        return true;
     }
     bool isRotated(string str1, string str2)
     {
         // Your code here
+        // TC - O(n)
+        // SC - O(n)
+        
         if(str1==str2)return 1;
         
         return antiClk(str1,str2)||clk(str1,str2);

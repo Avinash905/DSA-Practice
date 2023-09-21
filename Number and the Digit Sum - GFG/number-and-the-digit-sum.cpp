127ebc5d4ae1acc8657f54ed469846cb378a0d5b
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-09-21 16:25:34 +0530

    Added solution - LeetHub

--- Number and the Digit Sum - GFG/number-and-the-digit-sum.cpp
+++ Number and the Digit Sum - GFG/number-and-the-digit-sum.cpp
@@ -6,25 +6,22 @@ using namespace std;
 class Solution
 {
     public:
-        long long sumOfDigits(long long n){
-            long long sum=0; 
-            while(n){
-                sum+=n%10;
-                n/=10;
+        long long digitSum(long long mid){
+            long long sum=0;
+            while(mid){
+                sum+=mid%10;
+                mid/=10;
             }
             return sum;
         }
         long long int numberCount(long long int N, long long int K)
         {
             // your code here
-            // TC - O(log(n))
-            // SC - O(1)
-            
-            int start=1,end=N;
+            long long start=1,end=N;
             while(start<=end){
-                int mid=start+(end-start)/2;
+                long long mid=start+(end-start)/2;
                 
-                if(mid-sumOfDigits(mid)<K)start=mid+1;
+                if(mid-digitSum(mid)<K)start=mid+1;
                 else end=mid-1;
             }
             return N-end;

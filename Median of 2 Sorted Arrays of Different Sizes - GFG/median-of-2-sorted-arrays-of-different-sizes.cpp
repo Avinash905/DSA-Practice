91b7898aa4c7b33f661cb50d67c66f61abe42a5a
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-10-01 00:01:40 +0530

    Added solution - LeetHub

--- Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -11,29 +11,33 @@ double MedianOfArrays(vector<int>& array1, vector<int>& array2);
 
 class Solution{
     public:
+    double findMedian(vector<int> array1, vector<int> array2){
+        int n=array1.size(),m=array2.size(),start=0,end=n;
+
+        while(start<=end){
+            int m1=start+(end-start)/2,m2=(n+m+1)/2-m1;
+            
+            int mx1=m1==0?INT_MIN:array1[m1-1];
+            int mx2=m2==0?INT_MIN:array2[m2-1];
+            int mn1=m1==n?INT_MAX:array1[m1];
+            int mn2=m2==m?INT_MAX:array2[m2];
+            
+            if(mx1<=mn2 && mx2<=mn1){
+                if((n+m)%2==0)return (max(mx1,mx2)+min(mn1,mn2))/2.0;
+                else return max(mx1,mx2);
+            }
+            else if(mx1>mn2)end=m1-1;
+            else start=m1+1;
+        }
+        return 0;
+    }
     double MedianOfArrays(vector<int>& array1, vector<int>& array2)
     {
         // Your code goes here
-        // TC - O(n+m)
-        // SC - O(1)
+        int n=array1.size(),m=array2.size();
         
-        int i=0,j=0,n=array1.size(),m=array2.size(),first,second;
-        
-        for(int k=0;k<=(n+m)/2;k++){
-            first=second;
-            if(i<n && j<m){
-                if(array1[i]<array2[j]){
-                    second=array1[i++];
-                }
-                else if(array1[i]>=array2[j]){
-                    second=array2[j++];
-                }
-            }
-            else if(i<n)second=array1[i++];
-            else second=array2[j++];
-        }
-        
-        return (n+m)%2==0?(first+second)/2.0:second;
+        if(m>=n)return findMedian(array1,array2);
+        else return findMedian(array2,array1);
     }
 };
 

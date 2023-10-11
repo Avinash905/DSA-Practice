8b41c352c738f1ca53ee6bc3238fed6d62fb01a1
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-10-11 13:05:28 +0530

    Added solution - LeetHub

---
+++ Count number of elements between two given elements in array - GFG/count-number-of-elements-between-two-given-elements-in-array.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    int getCount(int arr[], int n, int num1, int num2)
+    {
+        //Complete the function
+        int left=n,right=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==num1)left=min(left,i);
+            if(arr[i]==num2)right=max(right,i);
+        }
+        if(left==right)return 0;
+        return right-left-1;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	    int num1,num2;
+	    cin>>num1>>num2;
+	    Solution ob;
+	    cout << ob.getCount(a, n, num1, num2) <<"\n";
+	}
+	return 0;
+}
+
+// } Driver Code Ends

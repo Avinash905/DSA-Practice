451109beebbe017893a4b4dddf919b60d1335aa6
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-10-19 11:53:03 +0530

    Added Solution - GfG to GitHub

---
+++ School/Count Digits/count-digits.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int count=0,temp=N;
+        while(N){
+            if(N%10 && temp%(N%10)==0)count++;
+            N/=10;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends

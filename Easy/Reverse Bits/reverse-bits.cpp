9f17ca5f42a340a2140a94535e82900f9cf05c0c
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-10-19 12:57:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long X) {
+        // code here
+        long long num=0,count=0;
+        
+        while(count<32){
+            long long rem=X&1;
+            num=num*2+rem;
+            
+            X=X>>1;
+            count++;
+        }
+        return num;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

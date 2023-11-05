3c9637b3512f3d12e82bd75a54460f7ff7757751
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-11-05 23:44:58 +0530

    Time: 7 ms (16.85%), Space: 10 MB (19.38%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size(),n=matrix[0].size(),st=0,end=m*n-1;
+    bool binarySearch(vector<int>& matrix,int target){
+        int st=0,end=matrix.size()-1;
 
         while(st<=end){
-            int mid=st+(end-st)/2,row=mid/n,col=mid%n;
+            int mid=st+(end-st)/2;
 
-            if(matrix[row][col]==target)return 1;
-            else if(matrix[row][col]>target)end=mid-1;
+            if(matrix[mid]==target)return 1;
+            else if(matrix[mid]>target)end=mid-1;
+            else st=mid+1;
+        }
+
+        return 0;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(),n=matrix[0].size(),st=0,end=m-1;
+
+        while(st<=end){
+            int mid=st+(end-st)/2,firstEle=matrix[mid][0],lastEle=matrix[mid][n-1];
+
+            if(lastEle==target)return 1;
+            else if(target>=firstEle && target<lastEle){
+                return binarySearch(matrix[mid],target);
+            }
+            else if(lastEle>target)end=mid-1;
             else st=mid+1;
         }
 

88b06527dd131bad95ebea8ecb448cede60b6bca
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-12-17 22:36:00 +0530

    Updated Solution - GfG to GitHub

--- Easy/Sieve of Eratosthenes/sieve-of-eratosthenes.cpp
+++ Easy/Sieve of Eratosthenes/sieve-of-eratosthenes.cpp
@@ -10,24 +10,15 @@ public:
     vector<int> sieveOfEratosthenes(int N)
     {
         // Write Your Code here
-        vector<int> freq;
-        
+        int notPrime[N+1]={0};
         for(int i=2;i<=N;i++){
-            freq.push_back(i);
-        }
-        for(int i=0;i<freq.size();i++){
-            int ele=freq[i];
-            if(ele>0){
-                for(int j=i+ele;j<freq.size();j+=ele){
-                    freq[j]=-1;
-                }
+            if(notPrime[i]==1)continue;
+            for(int j=2*i;j<=N;j+=i){
+                notPrime[j]=1;
             }
         }
-        
         vector<int> ans;
-        for(int i=0;i<freq.size();i++){
-            if(freq[i]>0)ans.push_back(freq[i]);
-        }
+        for(int i=2;i<=N;i++)if(notPrime[i]==0)ans.push_back(i);
         return ans;
     }
 };
